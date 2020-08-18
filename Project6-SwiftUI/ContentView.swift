043ae13d809f84a30e7a61e4ab99ab3eabda7c95
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-18 19:42:20 -0300

    Controlling the animation stack

--- Project6-SwiftUI/ContentView.swift
+++ Project6-SwiftUI/ContentView.swift
@@ -10,59 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount: Double = 0.0
+    @State private var enable = false
     
     var body: some View {
-//        Button("Tap me"){
-////            self.animationAmount += 1
-//        }
-//        .padding(50)
-//        .background(Color.red)
-//        .foregroundColor(.white)
-//        .clipShape(Circle())
-//        .overlay(
-//            Circle()
-//                .stroke(Color.red)
-//                .scaleEffect(animationAmount)
-//                .opacity(Double(2-animationAmount))
-//            .animation(
-//                       Animation.easeInOut(duration: 1)
-//                       .repeatForever(autoreverses: false)
-//                   )
-//        )
-//        .onAppear{
-//                self.animationAmount = 2
-//        }
-//
-//        print(animationAmount)
-//
-//        return VStack{
-//            Stepper("Scale amount", value: $animationAmount.animation(
-//                Animation.easeInOut(duration: 1)
-//                .repeatCount(3, autoreverses: true)
-//            ), in: 1...10)
-//
-//            Spacer()
-//            Button("Tap me") {
-//                self.animationAmount += 1
-//            }
-//        .padding(40)
-//            .background(Color.red)
-//            .foregroundColor(.white)
-//        .clipShape(Circle())
-//        .scaleEffect(animationAmount)
-//            }
-        
-        Button("Tap me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
-            }
+
+        Button("Tap me"){
+            self.enable.toggle()
         }
-    .padding(50)
-        .background(Color.red)
+        .frame(width:200, height: 200)
+        .background(enable ? Color.blue : Color.red)
+        .animation(nil)
         .foregroundColor(.white)
-    .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0) )
+        .animation(.default)
+        .clipShape(RoundedRectangle(cornerRadius: enable ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
         
   }
     
