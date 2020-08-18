fb0818d7ec0be9494e69ec64283c05a70ff46377
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-17 21:45:09 -0300

    Initial Commit

---
+++ Project6-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project6-SwiftUI
+//
+//  Created by Ezequiel Parada Beltran on 17/08/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
