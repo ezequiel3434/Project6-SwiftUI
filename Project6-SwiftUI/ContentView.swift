7a1742c3d71d50c50547e2465830fb90d35ba38c
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-17 21:48:26 -0300

    initial commit

--- Project6-SwiftUI/ContentView.swift
+++ Project6-SwiftUI/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Button("Tap me"){
+            
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        
     }
 }
 
